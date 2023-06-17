0e7d304a95dc34478bf8d5ef2bed9b5f9842fde3
aity-yahoo <136758308+aity-yahoo@users.noreply.github.com>
2023-06-17 05:28:22 +0200

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -1,31 +1,39 @@
 import telebot
 from telebot import types
-from telethon.sync import TelegramClient
-from telethon.tl.functions.messages import GetDialogsRequest
-from telethon.tl.types import InputPeerEmpty
+import configparser
 import csv
 import time
-import configparser
 
-def start_scraper(bot, message):
-    cpass = configparser.RawConfigParser()
-    cpass.read('config.data')
+cpass = configparser.RawConfigParser()
+cpass.read('config.data')
 
+bot = telebot.TeleBot('5681732028:AAErgYe8EPUMFz9kg4whvaHgefroADzr1fE')
+
+@bot.message_handler(commands=['scraper'])
+def scraper(message):
     try:
         api_id = cpass['cred']['id']
         api_hash = cpass['cred']['hash']
         phone = cpass['cred']['phone']
         client = TelegramClient(phone, api_id, api_hash)
     except KeyError:
-        bot.reply_to(message, "Primero ejecuta /setup")
+        bot.reply_to(message, "[!] Ejecute /setup primero.")
         return
-
+    
     client.connect()
     if not client.is_user_authorized():
         client.send_code_request(phone)
-        bot.reply_to(message, "Ingresa el código:")
-        return
+        msg = bot.reply_to(message, "Ingrese el código de verificación:")
+        bot.register_next_step_handler(msg, process_verification_code, client)
+    else:
+        scrape_members(message, client)
 
+def process_verification_code(message, client):
+    code = message.text
+    client.sign_in(phone, code)
+    scrape_members(message, client)
+
+def scrape_members(message, client):
     chats = []
     last_date = None
     chunk_size = 200
@@ -47,47 +55,45 @@ def start_scraper(bot, message):
         except:
             continue
 
-    bot.reply_to(message, "Elige un grupo para obtener los miembros:")
-    keyboard = types.ReplyKeyboardMarkup(one_time_keyboard=True)
+    markup = types.ReplyKeyboardMarkup(one_time_keyboard=True)
     for i, g in enumerate(groups):
-        keyboard.add(types.KeyboardButton(f'[{i}] - {g.title}'))
+        markup.add(types.KeyboardButton(f"[{i}] - {g.title}"))
+    msg = bot.reply_to(message, "Seleccione un grupo para extraer miembros:", reply_markup=markup)
+    bot.register_next_step_handler(msg, process_group_selection, groups)
 
-    msg = bot.send_message(message.chat.id, "Elige un grupo:", reply_markup=keyboard)
-    bot.register_next_step_handler(msg, process_group_selection, client, groups)
-
-
-def process_group_selection(message, client, groups):
+def process_group_selection(message, groups):
     try:
-        group_index = int(message.text[1:-1])  # Obtener el índice del grupo seleccionado
-        target_group = groups[group_index]  # Obtener el grupo seleccionado
+        group_index = int(message.text[1:-1])
+        target_group = groups[group_index]
+    except (ValueError, IndexError):
+        bot.reply_to(message, "Selección de grupo no válida.")
+        return
 
-        bot.reply_to(message, "Obteniendo miembros...")
-        time.sleep(1)
-        all_participants = []
-        all_participants = client.get_participants(target_group, aggressive=True)
-        
-        bot.reply_to(message, "Guardando en el archivo...")
-        time.sleep(1)
-        with open("members.csv", "w", encoding='UTF-8') as f:
-            writer = csv.writer(f, delimiter=",", lineterminator="\n")
-            writer.writerow(['username', 'user id', 'access hash', 'name', 'group', 'group id'])
-            for user in all_participants:
-                if user.username:
-                    username = user.username
-                else:
-                    username = ""
-                if user.first_name:
-                    first_name = user.first_name
-                else:
-                    first_name = ""
-                if user.last_name:
-                    last_name = user.last_name
-                else:
-                    last_name = ""
-                name = (first_name + ' ' + last_name).strip()
-                writer.writerow([username, user.id, user.access_hash, name, target_group.title, target_group.id])
+    bot.reply_to(message, "Obteniendo miembros...")
+    time.sleep(1)
+    all_participants = client.get_participants(target_group, aggressive=True)
 
-        bot.reply_to(message, "Miembros extraídos exitosamente.")
-    except IndexError:
-        bot.reply_to(message, "Índice de grupo inválido.")
+    bot.reply_to(message, "Guardando en el archivo...")
+    time.sleep(1)
+    with open("members.csv", "w", encoding='UTF-8') as f:
+        writer = csv.writer(f, delimiter=",", lineterminator="\n")
+        writer.writerow(['username', 'user id', 'access hash', 'name', 'group', 'group id'])
+        for user in all_participants:
+            if user.username:
+                username = user.username
+            else:
+                username = ""
+            if user.first_name:
+                first_name = user.first_name
+            else:
+                first_name = ""
+            if user.last_name:
+                last_name = user.last_name
+            else:
+                last_name = ""
+            name = (first_name + ' ' + last_name).strip()
+            writer.writerow([username, user.id, user.access_hash, name, target_group.title, target_group.id])
 
+    bot.reply_to(message, "Miembros extraídos exitosamente.")
+
+bot.polling()
