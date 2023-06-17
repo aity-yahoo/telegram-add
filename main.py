02e846fa7ed5917d949be2ca33c0de385601cfcf
aity-yahoo <136758308+aity-yahoo@users.noreply.github.com>
2023-06-17 04:17:24 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,22 +1,6 @@
-from flask import Flask
-import setup
-import scraper
-
-app = Flask(__name__)
-
-@app.route('/')
-def home():
-    return "Hello, World!"
-
-@app.route('/setup')
-def run_setup():
-    setup.setup()
-    return "Setup process started."
-
-@app.route('/scraper')
-def run_scraper():
-    scraper.scraper()
-    return "Scraper process started."
+from scraper import scraper
+from setup import setup
 
 if __name__ == '__main__':
-    app.run()
+    setup()
+    scraper()
