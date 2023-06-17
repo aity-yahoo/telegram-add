646c42dad76e4f19f7c223c42e3a8b10b9803a3b
aity-yahoo <136758308+aity-yahoo@users.noreply.github.com>
2023-06-17 03:50:54 +0200

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,42 @@
+import telebot
+from telebot import types
+import configparser
+
+bot = telebot.TeleBot('TOKEN')
+
+@bot.message_handler(commands=['start'])
+def start(message):
+    markup = types.ReplyKeyboardMarkup(one_time_keyboard=True)
+    bot.reply_to(message, "¡Bienvenido! Este bot te permite añadir usuarios a tu grupo de Telegram.", reply_markup=markup)
+
+@bot.message_handler(commands=['setup'])
+def setup(message):
+    markup = types.ReplyKeyboardMarkup(one_time_keyboard=True)
+    msg = bot.reply_to(message, "Ingrese su API ID:", reply_markup=markup)
+    bot.register_next_step_handler(msg, process_api_id)
+
+def process_api_id(message):
+    api_id = message.text
+    msg = bot.reply_to(message, "Ingrese su Hash ID:")
+    bot.register_next_step_handler(msg, process_hash_id, api_id)
+
+def process_hash_id(message, api_id):
+    hash_id = message.text
+    msg = bot.reply_to(message, "Ingrese su número de teléfono:")
+    bot.register_next_step_handler(msg, process_phone_number, api_id, hash_id)
+
+def process_phone_number(message, api_id, hash_id):
+    phone_number = message.text
+
+    cpass = configparser.RawConfigParser()
+    cpass.add_section('cred')
+    cpass.set('cred', 'id', api_id)
+    cpass.set('cred', 'hash', hash_id)
+    cpass.set('cred', 'phone', phone_number)
+
+    with open('config.data', 'w') as setup:
+        cpass.write(setup)
+
+    bot.reply_to(message, "Configuración guardada exitosamente.")
+
+bot.polling()
